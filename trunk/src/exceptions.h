13ceac7e647b4fe6553d527a959e3194c4e2a3d8
rws <rws@02b5959b-c81b-0410-ab36-edcb2f400e12>
2006-08-26 08:44:57 +0000

    add FileException and default ctor perror output for all classes

--- trunk/src/exceptions.h
+++ trunk/src/exceptions.h
@@ -8,12 +8,15 @@
  * Released under GNU GPL2, read the file 'COPYING' for more information.
  */
 
+#include <errno.h>
 #include <glibmm/ustring.h>
+#include <glibmm/stringutils.h>
+#include <glibmm/i18n.h>
 
 class PipeException 
 {
 public:
-	PipeException() { _str = _("Pipe problem: "); }
+	PipeException() { _str = _("Pipe problem: ") + Glib::strerror (errno); }
 	PipeException (const Glib::ustring& s) { _str += s; }
 
 	Glib::ustring _str;
@@ -22,8 +25,17 @@ public:
 class TempfileException 
 {
 public:
-	TempfileException() { _str = _("Tempfile problem: "); }
+	TempfileException() { _str = _("Tempfile problem: ") + Glib::strerror (errno); }
 	TempfileException (const Glib::ustring& s) { _str += s; }
+
+	Glib::ustring _str;
+};
+
+class FileException 
+{
+public:
+	FileException() { _str = _("File problem: ") + Glib::strerror (errno); }
+	FileException (const Glib::ustring& s) { _str += s; }
 
 	Glib::ustring _str;
 };
