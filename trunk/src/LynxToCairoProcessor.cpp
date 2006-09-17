207b9e94c1e534aca9824259b8a3509bb528c3a5
rws <rws@02b5959b-c81b-0410-ab36-edcb2f400e12>
2006-09-17 16:59:51 +0000

    handle <>& characters in dump

--- trunk/src/LynxToCairoProcessor.cpp
+++ trunk/src/LynxToCairoProcessor.cpp
@@ -6,7 +6,11 @@
  * Released under GNU GPL2, read the file 'COPYING' for more information.
  */
 
+//#include <iostream>
+
+#include <glib/gmessages.h>
 #include "LynxToCairoProcessor.h"
+
 
 LynxToCairoProcessor::LynxToCairoProcessor()
 {
@@ -21,6 +25,22 @@ LynxToCairoProcessor::set_uri (const Glib::ustring& uri)
 const Glib::ustring&
 LynxToCairoProcessor::process (const Glib::ustring& dump)
 {
-	return dump;
+	Glib::ustring* str = new Glib::ustring (dump);
+	size_t pos = 0;
+	while ((pos = str->find_first_of ("<>&"), pos) != 
+		static_cast<size_t>(-1))
+	{
+		switch (dump[pos]) 
+		{
+		case '<': str->replace (pos, 1, "&lt;", 4); break;
+		case '>': str->replace (pos, 1, "&gt;", 4); break;
+		case '&': str->replace (pos, 1, "&amp;", 5); break;
+		default: 
+			g_return_val_if_reached (*str);
+		}
+
+		++pos;
+	}
+	return *str;
 }
 
