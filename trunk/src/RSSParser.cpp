f06d1dba3b5b29ee7711e55737510c7dd1bf9b0d
rws <rws@02b5959b-c81b-0410-ab36-edcb2f400e12>
2006-10-16 16:32:33 +0000

    first stab at rss parser

--- trunk/src/RSSParser.cpp
+++ trunk/src/RSSParser.cpp
@@ -5,16 +5,98 @@
  * Released under GNU GPL2, read the file 'COPYING' for more information.
  */
 
+#include <iostream>
+#include <functional>
+#include <glib/gmessages.h>
+#include "Item.h"
+#include "AppContext.h"
 #include "RSSParser.h"
 
 RSSParser::RSSParser (const Glib::ustring& str)
-: xmlpp::DomParser()
+: xmlpp::SaxParser()
 {
-	xmlpp::DomParser::parse_memory (str);
+	_curr_string = NULL;
+	_channel = _in_item = false;
+	xmlpp::SaxParser::parse_memory (str);
 }
 
 RSSParser::~RSSParser() {}
 
+
+//-------------------------------------------------------------
+void RSSParser::on_start_document()
+{
+  std::cout << "on_start_document()" << std::endl;
+}
+
+void RSSParser::on_end_document()
+{
+  std::cout << "on_end_document()" << std::endl;
+}
+
+void RSSParser::on_start_element(const Glib::ustring& name,
+                                   const AttributeList& attributes)
+{
+	if (_in_item)
+	{
+		if (name == "title")
+			_curr_string = &_item->_title;
+		else if (name == "description")
+			_curr_string = &_item->_description;
+		else if (name == "pubDate")
+			_curr_string = &_item->_pubdate;
+	}
+	else if (name == "item")
+	{
+		_in_item = true;
+		_item = new Item;
+	}
+	if (name == "channel")
+	{
+		if (_channel)
+			g_warning ("Multiple channel elements: not implemented.\n");
+		_channel = true;
+	}
+	
+}
+
+void RSSParser::on_end_element(const Glib::ustring& name)
+{
+	if (name == "item")
+	{
+		_in_item = false;
+		_curr_string = NULL;
+		AppContext::get().add_item (_item);
+	}
+}
+
+void RSSParser::on_characters(const Glib::ustring& text)
+{
+	if (_curr_string != NULL)
+	{
+		*_curr_string = text;
+		_curr_string = NULL;
+	}
+}
+
+void RSSParser::on_comment(const Glib::ustring& text)
+{
+}
+
+void RSSParser::on_warning(const Glib::ustring& text)
+{
+  std::cout << "on_warning(): " << text << std::endl;
+}
+
+void RSSParser::on_error(const Glib::ustring& text)
+{
+  std::cout << "on_error(): " << text << std::endl;
+}
+
+void RSSParser::on_fatal_error(const Glib::ustring& text)
+{
+  std::cout << "on_fatal_error(): " << text << std::endl;
+}
 
 
 
--- trunk/src/RSSParser.h
+++ trunk/src/RSSParser.h
@@ -9,19 +9,41 @@
  */
 
 #include <glibmm/ustring.h>
-#include <libxml++/parsers/domparser.h>
+#include <libxml++/parsers/saxparser.h>
+
+class Item;
+
+typedef std::map<Glib::ustring,Glib::ustring> str_str_map_t;
+
 
 /// RSSParser
 
-class RSSParser : public xmlpp::DomParser
+class RSSParser : public xmlpp::SaxParser
 {
 public:
 	RSSParser (const Glib::ustring& str);
 	virtual ~RSSParser();
+	
+	Item* get_item() { return _item; }
 
 protected:
-};
+  //overrides:
+  virtual void on_start_document();
+  virtual void on_end_document();
+  virtual void on_start_element(const Glib::ustring& name,
+                                const AttributeList& properties);
+  virtual void on_end_element(const Glib::ustring& name);
+  virtual void on_characters(const Glib::ustring& characters);
+  virtual void on_comment(const Glib::ustring& text);
+  virtual void on_warning(const Glib::ustring& text);
+  virtual void on_error(const Glib::ustring& text);
+  virtual void on_fatal_error(const Glib::ustring& text);
 
+// 	str_str_map_t	_stringmap;
+	Glib::ustring	*_curr_string;
+	Item		*_item;
+	bool		_channel, _in_item;
+};
 
 
 #endif
