28cfc3a27eb4698efe419c14e550614a1ae0ff54
rws <rws@02b5959b-c81b-0410-ab36-edcb2f400e12>
2006-10-22 09:11:52 +0000

    add MiscException, derive exceptions from Glib::Exception

--- trunk/src/exceptions.h
+++ trunk/src/exceptions.h
@@ -10,32 +10,53 @@
 
 #include <errno.h>
 #include <glibmm/ustring.h>
+#include <glibmm/exception.h>
 #include <glibmm/stringutils.h>
 #include <glibmm/i18n.h>
 
-class PipeException 
+class PipeException : public Glib::Exception
 {
 public:
 	PipeException() { _str = _("Pipe problem: ") + Glib::strerror (errno); }
-	PipeException (const Glib::ustring& s) { _str += s; }
+	PipeException (const Glib::ustring& s) { _str = s; }
+
+	virtual ~PipeException() throw() {}
+	virtual Glib::ustring what() const { return _str; }
 
 	Glib::ustring _str;
 };
 
-class TempfileException 
+class TempfileException  : public Glib::Exception
 {
 public:
 	TempfileException() { _str = _("Tempfile problem: ") + Glib::strerror (errno); }
-	TempfileException (const Glib::ustring& s) { _str += s; }
+	TempfileException (const Glib::ustring& s) { _str = s; }
+
+	virtual ~TempfileException() throw() {}
+	virtual Glib::ustring what() const { return _str; }
 
 	Glib::ustring _str;
 };
 
-class FileException 
+class FileException  : public Glib::Exception
 {
 public:
 	FileException() { _str = _("File problem: ") + Glib::strerror (errno); }
-	FileException (const Glib::ustring& s) { _str += s; }
+	FileException (const Glib::ustring& s) { _str = s; }
+
+	virtual ~FileException() throw() {}
+	virtual Glib::ustring what() const { return _str; }
+
+	Glib::ustring _str;
+};
+
+class MiscException  : public Glib::Exception
+{
+public:
+	MiscException (const Glib::ustring& s) { _str = _("Problem encountered: ") + s; }
+
+	virtual ~MiscException() throw() {}
+	virtual Glib::ustring what() const { return _str; }
 
 	Glib::ustring _str;
 };
