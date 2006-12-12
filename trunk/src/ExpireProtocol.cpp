1851419ebee46dacbef9381b2f13d1d669bddb8a
rws <rws@02b5959b-c81b-0410-ab36-edcb2f400e12>
2006-12-12 18:24:13 +0000

    add expire protocol

---
+++ trunk/src/ExpireProtocol.cpp
@@ -0,0 +1,40 @@
+/* \file
+ * Implementing the Expire Protocol.
+ *
+ * Copyright (c) 2006 R Stephan <ralf@ark.in-berlin.de>
+ * Released under GNU GPL2, read the file 'COPYING' for more information.
+ */
+
+#include "ExpireProtocol.h"
+#include "AppContext.h"
+#include "AppWindow.h"
+#include "FeedListColumnRecord.h"
+#include "Feed.h"
+
+
+namespace ExpireProtocol {
+
+/// Helper for expiring items which is called per feed
+/// TODO: do not call expire_items() twice per feed, see also AppContext::save()
+static bool
+expire_in_feed (const Gtk::TreeModel::Path& path, const Gtk::TreeModel::iterator& it)
+{
+	const FeedListColumnRecord& cr = FeedListColumnRecord::get();
+	Feed* feed = ((*it)[cr._col_feed]);
+    const unsigned int days = AppContext::get().get_expire();
+	feed->expire_items (days);
+	return false;
+}
+
+void
+do_expire()
+{
+    AppWindow *aw = AppContext::get().get_appwindow();
+	Glib::RefPtr<Gtk::TreeModel> tmodel = aw->get_feedlist()->get_tmodel();
+	tmodel->foreach (sigc::ptr_fun (&expire_in_feed));
+}
+
+
+} // namespace
+
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=4:softtabstop=4 :
---
+++ trunk/src/ExpireProtocol.h
@@ -0,0 +1,20 @@
+#ifndef __EXPIRE_PROTOCOL_H
+#define __EXPIRE_PROTOCOL_H
+
+/* \file
+ * Declaration of Expire Protocol.
+ *
+ * Copyright (c) 2006 R Stephan <ralf@ark.in-berlin.de>
+ * Released under GNU GPL2, read the file 'COPYING' for more information.
+ */
+
+
+namespace ExpireProtocol
+{
+    void do_expire();
+}
+
+#endif
+
+
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=4:softtabstop=4 :
