7342e43009b7e8fc305cc34199c81f80f13af71a
rws <rws@02b5959b-c81b-0410-ab36-edcb2f400e12>
2006-12-12 11:23:06 +0000

    widget cosmetics, set focus

--- trunk/src/ExpireDialog.cpp
+++ trunk/src/ExpireDialog.cpp
@@ -5,25 +5,44 @@
  * Released under GNU GPL2, read the file 'COPYING' for more information.
  */
 
+#include <list>
 #include <glibmm/i18n.h>
 #include <gtkmm/stock.h>
+#include <gtkmm/box.h>
 #include "ExpireDialog.h"
+#include "gtkutils.h"
 
+typedef std::list<const Gtk::Widget*> cwidget_list_t;
 
 ExpireDialog::ExpireDialog()
-    : Gtk::Dialog(_("Expire Items"), true), 
-    _spinner (1.0, 1),
+    : Gtk::Dialog(_("Expire Items"), true, true), 
+    _spinner (1.0, 0),
     _label1 (_("Minimum age of\nitems to be expired:")),
     _label2 (_("days"))
 {
-    add (_label1);
-    _box.add (_spinner);
-    _box.add (_label2);
-    add (_box);
-    add_button (Gtk::Stock::YES, 1);
-    add_button (Gtk::Stock::NO,  0);
+    _spinner.set_numeric (true);
+    _spinner.set_digits (1);
+    _spinner.set_range (0, 30);
+    _spinner.set_snap_to_ticks();
+
+    Gtk::VBox *vbox = get_vbox();
+    vbox->pack_start (_label1, Gtk::PACK_SHRINK, 10);
+    _box.add (*new Gtk::HBox);
+    _box.pack_start (_spinner, Gtk::PACK_SHRINK, 10);
+    _box.pack_start (_label2, Gtk::PACK_SHRINK, 0);
+    _box.add (*new Gtk::HBox);
+    vbox->pack_start (_box, Gtk::PACK_EXPAND_PADDING, 10);
+
+    add_button (_("Do expire"), 1);
+    add_button (Gtk::Stock::CANCEL, 0);
+
+    /// Move focus to Do button
+    cwidget_list_t list = get_action_area()->get_children();
+    for (cwidget_list_t::iterator it = list.begin(); it != list.end(); ++it)
+        if (get_response_for_widget (**it) == 1)
+        { const_cast<Widget&>(**it).grab_focus(); break; }
     
-    run();
+    show_all();
 }
 
 ExpireDialog::~ExpireDialog()
